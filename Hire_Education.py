07895dd8172d7e7956bb8d66d35ce2b03305aaa8
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2021-07-10 02:16:45 +0800

    Update Hire_Education.py

--- Hire_Education.py
+++ Hire_Education.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-warnings.filterwarnings('ignore')
 import base64
 import spacy
 import re
