8735522fd498d766f3e4c3da306df0b058ec3c56
Heide Balcera <77626580+Heide-B@users.noreply.github.com>
2021-07-10 12:11:55 +0800

    Update Hire_Education.py

--- Hire_Education.py
+++ Hire_Education.py
@@ -10,7 +10,6 @@ nlp = spacy.load("en_core_web_sm")
 df_all = pd.read_csv("courses_clean.csv", index_col=0)
 df = pd.read_csv("combined.csv")
 
-
 def find_job(response, job_titles):
     jobs = pd.DataFrame()
     jobs = df[df['Dominant_Topic'] == response].reset_index()
@@ -22,21 +21,45 @@ def find_job(response, job_titles):
 def search_courses(to_search):
     search = df[df['title'] == to_search]['Dominant_Topic'].unique()
     search = ''.join(search)
+    ## Input search keyword here
     search_in = nlp(search.lower())
     
     if search == 'Data Engineering':
         search_terms = ['developer','analyst','sap','engineer','java','sql','.net','manager','salesforce','web']
     elif search == 'Modelling/Analysis':
-        search_terms = ['use','product','least','modeling','analytical','portfolio','statistic','excel','environment','machine']
+        search_terms = ['modeling','analytical','statistic','excel','environment','machine']
     elif search == 'Data Storytelling':
-        search_terms = ['machine','use','visualization','portfolio','python','product','sql','tableau','vendor','member']
+        search_terms = ['machine','visualization','portfolio','python','product','sql','tableau']
     elif search == 'Business Application':
-        search_terms = ['marketing','update','evaluation','resolve','message','budgets','upload','activities','kpis forecast','predict']
+        search_terms = ['marketing','evaluation','budgets','kpis forecast','predict']
+
+    ## Getting the nlp similarity score of each term in "search_terms" and "search_in" 
+#    results = pd.DataFrame()
+#    for term in search_terms:
+#        t = nlp(term)
+#        score = t.similarity(search_in)
+#        t = {'term': term,'score':score}
+#        results = results.append(t, ignore_index = True)
+#    top_terms = results.sort_values(by="score",ascending=False).head(3)["term"].values.tolist()
+
+    ## The top 3 skills based on similarity to aid in filtering
+#    courses = pd.DataFrame()
+#    for term in top_terms:
+#        tmp = df_all[df_all["skill"] == term]
+#        courses = pd.concat([courses,tmp],ignore_index=True)
+
+    ## Filtering the courses in two steps. 
+    ## [1] Get the similarity of search item and the course title,
+    ## [2] Sort courses based on comments_score and rating
+    ## [3] Sort courses based on similarity score and drop duplicates based on title
+    
     
     for term in search_terms:
         t = nlp(term)
         for i in range(len(df_all)):
-            df_all['similarity'].iloc[i] = t.similarity(df_all['title'].iloc[i])
+            df_all['similarity'] = 0.0
+            df_all['similarity'].iloc[i] = t.similarity(nlp(df_all['title'].iloc[i]))
+#    df_all['similarity'] = df_all['title'].apply(lambda x: nlp(x.lower()).similarity(search_terms[1]) )
     
     similarity_df = df_all.sort_values(by=["comments_score","rating"],ascending=False)
     similarity_df = similarity_df.sort_values(by=["similarity"],ascending=False).drop_duplicates(subset=["title"]).head(3)
@@ -118,11 +141,12 @@ elif my_page == 'Our Methods':
 elif my_page == 'Hire Education Recommender':
     st.title("Hire Education Recommender Engine")
     st.header("Learn the skills you need for the job you want")
+    titles=[]
 
     response = st.selectbox('Choose Your Field of Specialization', ['','Data Engineering','Modelling/Analysis','Data Storytelling','Business Application'])
     if response != '':
         job_titles=[]
-        find_job(response, job_titles)
+        job_titles = find_job(response, job_titles)
         user_input = st.selectbox('Choose Your Potential Job!', job_titles)
         "Here are some courses to help you get hired as a/n: ", user_input
         if user_input != '':
